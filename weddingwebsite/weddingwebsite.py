7c9525b24390c665d09a9b37c4397ddee9bf4ea6
christophercacciatore <cg.cacciatore@gmail.com>
2017-02-18 15:49:28 -0600

    working html5up

--- weddingwebsite/weddingwebsite.py
+++ weddingwebsite/weddingwebsite.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import *
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Alexa and Chris are getting married!</h1>"
 
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
