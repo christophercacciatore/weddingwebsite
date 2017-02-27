2d3c9133ffc351b267106c26e3e8fb9a82c5c152
christophercacciatore <cg.cacciatore@gmail.com>
2017-02-26 21:23:05 -0600

    buttons for registry

--- weddingwebsite/weddingwebsite.py
+++ weddingwebsite/weddingwebsite.py
@@ -1,13 +1,10 @@
 from flask import *
 
 app = Flask(__name__)
-app.config.from_object('config')
 
 @app.route("/", methods = ['GET'])
 def index():
     return render_template('index.html')
-
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
