183d8dd9257dbc819de600f5c907768b03f2665b
christophercacciatore <cg.cacciatore@gmail.com>
2017-04-21 14:22:36 -0500

    just changed the photo

--- weddingwebsite/weddingwebsite.py
+++ weddingwebsite/weddingwebsite.py
@@ -1,10 +1,10 @@
-from flask import *
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route("/", methods = ['GET'])
 def index():
-    return render_template('index.html')
+    return flask.render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
