fab926e4afd8211e79f424f241ca199e8648fda1
christophercacciatore <cg.cacciatore@gmail.com>
2017-05-02 11:49:28 -0500

    autoplay for a pseudofix of the intialization problem

--- weddingwebsite/static/assets/js/slick_initializer.js
+++ weddingwebsite/static/assets/js/slick_initializer.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
   $('.slideshow').slick({
-	  speed: 300,
-	  initialSlide: 2,
-	  lazyLoad: 'ondemand',
+	  autoplay: true,
+  	  autoplaySpeed: 4000,
 	  infinite: true
   });
 });
 
+$(document).ready(function () {
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    $('.slideshow').slick('setPosition');
+  });
+});
